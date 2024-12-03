f5ba5727eb3cef2da68dcaaabf031f5be73f1643
unknown <k3kc228@gmail.com>
2024-12-03 13:12:16 +0600

    Store and slice have been added, as well as data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import styles from './styles/App.module.css'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setProducts } from './features/products/productsSlice';
+import productsData from './data/products.json'
+import styles from './styles/App.module.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setProducts(productsData));
+  }, [dispatch]);
+
   return (
-    <div className={styles.head}>
-      React App
+    <div>
+      <h1 className={styles.title}>Product List</h1>
+      {productsData.map(e => (
+        <div>{e.name}</div>
+      ))
+      }
     </div>
   );
 }
