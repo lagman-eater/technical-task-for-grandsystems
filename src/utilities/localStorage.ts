188dc7f061608e907b09f4eded859ace38296c84
unknown <k3kc228@gmail.com>
2024-12-03 14:05:56 +0600

    LocalStorage logic added

--- src/utilities/localStorage.ts
+++ src/utilities/localStorage.ts
@@ -1,1 +1,8 @@
-export default {}
+export const saveToLocalStorage = (key: string, value: any) => {
+    localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const loadFromLocalStorage = (key: string) => {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : null;
+};
