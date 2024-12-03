512f3f811f9add813a8b010885f10bba7c9ad800
unknown <k3kc228@gmail.com>
2024-12-03 14:37:33 +0600

    Added Product Item and Product List

--- src/features/products/ProductItem.tsx
+++ src/features/products/ProductItem.tsx
@@ -1,1 +1,40 @@
-export default {}
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleFavorite } from './productsSlice'
+import { RootState } from '../../app/store';
+
+interface Product {
+  id: number;
+  name: string;
+  category: string;
+  price: number;
+  image: string;
+  isFavorite: boolean;
+}
+
+interface ProductItemProps {
+  product: Product;
+}
+
+const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
+  const dispatch = useDispatch();
+  const isFavorite = useSelector((state: RootState) => state.products.favorites.includes(product.id));
+
+  const handleFavoriteToggle = () => {
+    dispatch(toggleFavorite(product.id)); 
+  };
+
+  return (
+    <div className="product-item">
+      <img src={product.image} alt={product.name} />
+      <h3>{product.name}</h3>
+      <p>{product.category}</p>
+      <p>${product.price}</p>
+      <button onClick={handleFavoriteToggle}>
+        {isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}
+      </button>
+    </div>
+  );
+};
+
+export default ProductItem;
--- src/features/products/ProductsList.tsx
+++ src/features/products/ProductsList.tsx
@@ -1,1 +1,25 @@
-export default {}
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setProducts } from './productsSlice';
+import { RootState } from '../../app/store';
+import ProductItem from './ProductItem';
+import productData from '../../data/products.json'
+
+const ProductsList: React.FC = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state: RootState) => state.products.filteredItems);
+
+  useEffect(() => {
+      dispatch(setProducts(productData))
+  }, [dispatch]);
+
+  return (
+    <div className="products-list">
+      {products.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductsList;
